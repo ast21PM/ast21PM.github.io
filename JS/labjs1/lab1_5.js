da4cc2ca7e837eaa72f7b4c6b33f8be6d2a5aff6
ast <161714238+ast21PM@users.noreply.github.com>
2024-05-06 09:40:36 +0300

    fix 5

--- JS/labjs1/lab1_5.js
+++ JS/labjs1/lab1_5.js
@@ -1,36 +1,29 @@
 'use strict';
 
-function drawTable(rows) {
-    let dogs = 3;
-    let cats = 2;
-    let table = '';
+let n = prompt("Введите число n:");
 
+function generateTable(rows) {
+    let table = '';
     for (let i = 0; i < rows; i++) {
         let row = '';
         for (let j = 0; j < 6; j++) {
-            if (dogs) {
+            if ((i + j) % 5 < 3) {
                 row += 'dog\t';
-                dogs--;
             } else {
                 row += 'cat\t';
-                cats--;
             }
         }
-
-        dogs = 3;
-        cats = 2;
         table += row + '\n';
     }
-
     console.log(table);
     alert(table);
 }
 
-let numRows = parseInt(prompt("Введите количество строк:", ""));
+let numRows = parseInt(n);
 
 if (isNaN(numRows) || numRows <= 0) {
-    console.log("Введите корректное значение количества строк.");
-    alert("Введите корректное значение количества строк.");
+    console.log("Введите корректное число n.");
+    alert("Введите корректное число n.");
 } else {
-    drawTable(numRows);
+    generateTable(numRows);
 }
